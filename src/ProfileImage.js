6ed8d4aefeb3f63026c4f5b7a4005b4f0ad273be
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2021-06-21 11:36:19 -0700

    upload progress bar set up

--- src/ProfileImage.js
+++ src/ProfileImage.js
@@ -4,20 +4,31 @@ import { uploadImage, getDownloadUrl } from './firebase/user';
 export const ProfileImage = ({ id }) => {
   const fileInput = useRef(null);
   const [imageUrl, setImageUrl] = useState('');
+  const [uploadProgress, setUploadProgress] = useState(0);
 
   useEffect(() => {
     getDownloadUrl(id).then((url) => !!url && setImageUrl(url));
   }, [id]);
-
+  
   const fileChange = async (files) => {
-    const ref = await uploadImage(id, files[0]);
+    const ref = await uploadImage(id, files[0], updateProgress);
     const downloadUrl = await ref.getDownloadURL();
     setImageUrl(downloadUrl);
   };
 
+  const updateProgress = (snapshot) => {
+    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+    setUploadProgress(progress);
+  };
+
+
   return (
     <div className="four wide column profile-image">
-      <img className="ui image" src={imageUrl || '/profile-placeholder.png'} alt="profile" />
+      <img
+        className="ui image"
+        src={imageUrl || '/profile-placeholder.png'}
+        alt="profile"
+      />
       <input
         className="file-input"
         type="file"
@@ -25,6 +36,11 @@ export const ProfileImage = ({ id }) => {
         ref={fileInput}
         onChange={(e) => fileChange(e.target.files)}
       />
+      <progress
+        style={{ width: '100%' }}
+        max="100"
+        value={uploadProgress}
+      ></progress>
       <button
         className="ui grey button upload-button"
         onClick={() => fileInput.current.click()}
